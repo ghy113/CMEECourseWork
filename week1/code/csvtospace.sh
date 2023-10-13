56443c0a18b84e7b3eabb2a39d6d588d9b87210c
ghy113 <hg2423@ic.ac.uk>
2023-10-13 10:27:57 +0100

    Full first commit of new project

---
+++ week1/code/csvtospace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Check if the correct number of arguments is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 filename.csv"
+    exit 1
+fi
+# Check if the input file exists
+if [ ! -f "$1" ]; then
+    echo "Input file not found: $1"
+    exit 1
+fi
+
+# Define the output file name
+output_file="${1%.csv}_space.csv"
+
+# replace , to space
+sed 's/,/ /g' "$1" > "$output_file"
+
+echo "Conversion complete. Saved as $output_file"
